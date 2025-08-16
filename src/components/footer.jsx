6b9398d380160735220eda99515b62839248b2f9
therealcc <carson.cassidy20@gmail.com>
2025-08-16 14:09:32 -0400

    Updated Home Page

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -2,7 +2,7 @@ export default function Footer({ children }) {
   return (
     <footer className="footer footer-center fixed bottom-0 left-0 z-20 w-full p-4 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 text-base-content">
       <div className="text-slate-100">
-        <p>Copyright © 2023 - Carson Cassidy</p>
+        <p>Copyright © 2025 - Carson Cassidy</p>
       </div>
     </footer>
   );
